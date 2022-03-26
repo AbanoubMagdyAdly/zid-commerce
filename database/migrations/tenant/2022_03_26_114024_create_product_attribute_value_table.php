e104b9710ca7a568ac6cd078b276c7ba813062d6
Abanoub Magdy Adly <abanoub.magdy.adly@gmail.com>
2022-03-26 14:07:23 +0200

    Add database schema

---
+++ database/migrations/tenant/2022_03_26_114024_create_product_attribute_value_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductAttributeValueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_attribute_value', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id');
+            $table->foreignId('attribute_id');
+            $table->string('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_attribute_value');
+    }
+}
